14508922bf78f0754870affa919d2c77d5f483ab
jolwa-welkin <101490081+jolwa-welkin@users.noreply.github.com>
2024-08-20 18:06:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-function translateWord() {
+async function translateWord() {
     const inputWord = document.getElementById("search").value.toLowerCase();
-    const table = document.getElementById("dictionaryTable");
-    const rows = table.getElementsByTagName("tr");
-    let translation = null;
-
-    for (let i = 0; i < rows.length; i++) {
-        const cells = rows[i].getElementsByTagName("td");
-        const word = cells[0].textContent.toLowerCase();
-        const translatedWord = cells[1].textContent;
-
-        if (word === inputWord) {
-            translation = translatedWord;
-            break;
-        }
-    }
-
+    
+    // Fetch the JSON file and convert it to a JavaScript object
+    const response = await fetch('dictionary.json');
+    const dictionary = await response.json();
+    
+    const translation = dictionary[inputWord];
     const resultElement = document.getElementById("result");
+
     if (translation) {
         resultElement.textContent = `Translation: ${translation}`;
     } else {
